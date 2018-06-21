2201234736912617b0ca9f729f78557265cb4452
damanjeetmar28@gmail.com <damanjeetmar28@gmail.com>
2018-06-21 22:29:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ function shout(string){
 }
 
 function sayHiToGrandma(string){
-  
+  if(string.toLowerCase() === string) {
+    console.log("I can't hear you!");
+  }
+  else if(string.toUpperCase() === string) {
+    console.log()
+  }
 }
